e294b969235571817074742c2c14661bfd716b26
Phil Weir <phil.weir@flaxandteal.co.uk>
2023-03-07 16:25:22 +0000

    standardize enum behaviour

--- arches_graphql_client/dask.py
+++ arches_graphql_client/dask.py
@@ -37,7 +37,7 @@ def bulk_create_from_groups(
     with Client(cluster) as dask_client:
         dask_client.submit(_install).result()
         for n, group in enumerate(groups):
-            print("{n} / {len(groups)}")
+            print(f"{n} / {len(groups)}")
             fut = dask_client.submit(execute, ENDPOINT, group, model, model_name)
             if detach:
                 dask.distributed.fire_and_forget(fut)
--- arches_graphql_client/utils.py
+++ arches_graphql_client/utils.py
@@ -1,15 +1,27 @@
+import re
 from functools import partial
+import slugify
+
+def snake(class_name):
+    class_name = class_name[0].lower() + class_name[1:]
+    if class_name[0] == "_" and class_name[1].isnumeric():
+        class_name = class_name[1:]
+    class_name = class_name[0] + re.sub("([a-zA-Z])([A-Z])", r"\1_\2", class_name[1:])
+    return class_name.lower()
+
+
+def string_to_enum(string):
+    string = string.replace("& ", "and ")
+    if "/" in string:
+        return "".join(map(string_to_enum, string.split("/")))
+    return studly(slugify.slugify(string.replace(" ", "-")).replace("-", " "))
 
 
 def camel(string, studly=False):
-    string = string.replace("& ", "and ").replace("-", " ")
-    string = (
-        (string[0].upper() if studly else string[0].lower())
-        + string.replace("_", " ").title()[1:]
-    ).replace(" ", "")
+    string = ((string[0].upper() if studly else string[0].lower()) + string.replace("_", " ").title()[1:]).replace(" ", "")
     if string[-1] != string[-1].lower():
         string += "_"
+    if string[0].isnumeric():
+        string = "_" + string
     return string
-
-
 studly = partial(camel, studly=True)
